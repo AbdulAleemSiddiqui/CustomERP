0be81a6186ff5ec8fd95f6b1f62feaa1fe52dbc5
Fasih <smfasihali@gmail.com>
2019-03-21 21:05:18 +0500

    COA All Views and action withoout DB

---
+++ SfDesk/Controllers/ChartOfAccountController.cs
@@ -0,0 +1,41 @@
+﻿using SfDesk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SfDesk.Controllers
+{
+    public class ChartOfAccountController : Controller
+    {
+        // GET: ChartOfAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(ChartOfAccount c)
+        {
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            return PartialView("Detail", new ChartOfAccount().COA_Get_By_ID(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(ChartOfAccount c)
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ SfDesk/Models/Account_Type.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SfDesk.Models
+{
+    public class Account_Type
+    {
+        public int Type_ID { get; set; }
+        public string Type_Name{ get; set; }
+    }
+}
