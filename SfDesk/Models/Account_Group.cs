8271b7a94f5ebe8746663f4e5ec74fc33ca5c9bd
Fasih <smfasihali@gmail.com>
2019-03-22 16:20:24 +0500

    type group

--- SfDesk/Models/Account_Group.cs
+++ SfDesk/Models/Account_Group.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +11,23 @@ namespace SfDesk.Models
         public int G_ID { get; set; }
         public string G_Name { get; set; }
         public string Nature { get; set; }
+
+        public List<Account_Group> Account_Group_Get_All()
+        {
+            List<Account_Group> lst = new List<Account_Group>();
+            SqlCommand sc = new SqlCommand("Account_Group_Get_All", Connection.Get()) { CommandType = System.Data.CommandType.StoredProcedure }; ;
+            sc.Parameters.AddWithValue("@App_Id", App.App_ID);
+            SqlDataReader sdr = sc.ExecuteReader();
+            while (sdr.Read())
+            {
+                Account_Group u = new Account_Group();
+                u.G_ID = (int)sdr["Group_ID"];
+                u.G_Name = (string)sdr["G_Name"];
+                lst.Add(u);
+
+            }
+            sdr.Close();
+            return lst;
+        }
     }
 }
