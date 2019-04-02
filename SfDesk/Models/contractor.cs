a78c9190795c51a536364242fb6077f1e4f2eb31
a.aleem <SF Desk@DESKTOP-8RVHAEQ>
2019-04-02 15:55:02 +0500

    Model work

--- SfDesk/Models/contractor.cs
+++ SfDesk/Models/contractor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -12,8 +13,12 @@ namespace SfDesk.Models
         public string C_Name { get; set; }
         public decimal C_Amount { get; set; }
         public string C_Unit { get; set; }
-        public string Machine_Ip { get; set; }
 
+        public int Created_By { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Created_Date { get; set; } = DateTime.Parse("2001/01/01");
+        public string Machine_Ip { get; set; }
         public string Mac_Address { get; set; }
 
         public Contractor()
@@ -23,7 +28,25 @@ namespace SfDesk.Models
         }
         public List<Contractor> Contractor_Get_All()
         {
-            return new List<Contractor>();
+            List<Contractor> lst = new List<Contractor>();
+            SqlCommand sc = new SqlCommand("Contractor_Get_All", Connection.Get()) { CommandType = System.Data.CommandType.StoredProcedure };
+            sc.Parameters.AddWithValue("@CreatedBy", App.App_ID);
+            SqlDataReader sdr = sc.ExecuteReader();
+            while (sdr.Read())
+            {
+                Contractor u = new Contractor();
+                u.C_ID = (int)sdr["C_Name"];
+                u.C_Name= (string)sdr["C_Name"];
+                u.C_Amount= (decimal)sdr["C_Amount"];
+                u.C_Unit = (string)sdr["C_Unit"];
+                u.Created_By = (int)sdr["CreatedBy"];
+                u.Created_Date = (DateTime)sdr["CreatedDate"];
+                u.Machine_Ip = (string)sdr["Machine_Ip"];
+                u.Mac_Address = (string)sdr["Mac_Address"];
+                lst.Add(u);
+            }
+            sdr.Close();
+            return lst;
         }
         public Contractor Contractor_Get_By_ID()
         {
