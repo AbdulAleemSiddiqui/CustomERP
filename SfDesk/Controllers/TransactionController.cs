0308fbbb38bd8ced26bfbfe05be0fff62baf84e4
Fasih <smfasihali@gmail.com>
2019-05-18 12:32:01 +0500

    transaction

---
+++ SfDesk/Controllers/TransactionController.cs
@@ -0,0 +1,44 @@
+﻿using SfDesk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SfDesk.Controllers
+{
+    public class TransactionController : Controller
+    {
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            return View(new Transaction().Transaction_Get_All());
+        }
+        public ActionResult Add()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Transaction s)
+        {
+            s.Transaction_Add();
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            return PartialView("Edit", new Transaction() { T_ID = id }.Transaction_Get_By_ID());
+        }
+        [HttpPost]
+        public ActionResult Edit(Transaction c)
+        {
+            c.Transaction_Update();
+            return RedirectToAction("index");
+        }
+        public ActionResult Delete(int id)
+        {
+            new Transaction() {T_ID = id }.Transaction_Delete();
+            return RedirectToAction("index");
+        }
+    }
+}
