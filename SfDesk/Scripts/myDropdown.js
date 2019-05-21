0174c68d1b4aa426b31f51580b9007d99f29b653
Fasih <smfasihali@gmail.com>
2019-05-21 16:44:52 +0500

    123

---
+++ SfDesk/Scripts/myDropdown.js
@@ -0,0 +1,11 @@
+﻿
+     function valueSelect(index, input) {
+        var input = $('#' + input)
+        var list = $("#" + input.attr("list"))
+        var options = $($(list).prop("options"))
+        input.val(options.eq(index).val())
+ };//your stuff
+
+   
+   
+   
