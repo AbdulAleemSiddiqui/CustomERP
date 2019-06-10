6ddb6ff0edda703d18e60268a12df0255022834d
Fasih <smfasihali@gmail.com>
2019-06-10 20:16:03 +0500

    yoyo

--- SfDesk/Models/GRN.cs
+++ SfDesk/Models/GRN.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +10,22 @@ namespace SfDesk.Models
     public class GRN
     {
         public int GRN_NO { get; set; }
+        [DisplayName("Suplier")]
         public int Suplier_ID { get; set; }
         public String Suplier_Name { get; set; }
+        [DisplayName("Trans. NO")]
+        public int Trans_NO { get; set; }
+        [DisplayName("Trans. Date")]
+        [DataType(DataType.Date)]
         public DateTime Trans_Date { get; set; }
+        [DisplayName("Due Date")]
+        [DataType(DataType.Date)]
         public DateTime Due_Date { get; set; }
+        public string Currency { get; set; }
+        [DisplayName("Ref. No")]
         public int Ref_No { get; set; }
-        public string Narration { get; set; }
-        public int PI_ID{ get; set; }
+        [DisplayName("Ex. Rate")]
+
+        public decimal Exchange_Rate { get; set; }
     }
 }
