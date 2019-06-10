3024ca87439e549d64a52919544ec338cb62d9d3
Fasih <smfasihali@gmail.com>
2019-06-10 18:48:10 +0500

    GRN

---
+++ SfDesk/Controllers/GRNController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SfDesk.Controllers
+{
+    public class GRNController : Controller
+    {
+        // GET: GRN
+        public ActionResult Master()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ SfDesk/Models/GRN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SfDesk.Models
+{
+    public class GRN
+    {
+        public int GRN_NO { get; set; }
+        public int Suplier_ID { get; set; }
+        public String Suplier_Name { get; set; }
+        public DateTime Trans_Date { get; set; }
+        public DateTime Due_Date { get; set; }
+        public int Ref_No { get; set; }
+        public string Narration { get; set; }
+        public int PI_ID{ get; set; }
+    }
+}
