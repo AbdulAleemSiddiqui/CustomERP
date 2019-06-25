b0d8abd5df1cd95528b4895bd205391339c7dd28
a.aleem <SF Desk@DESKTOP-8RVHAEQ>
2019-06-25 22:20:49 +0500

    middleman

--- SfDesk/Models/MiddleMan.cs
+++ SfDesk/Models/MiddleMan.cs
@@ -60,6 +60,42 @@ namespace SfDesk.Models
                 u.Exp_Acc_Name = (string)sdr["Exp_Acc_Name"];
                 u.Pay_Acc_ID = (int)sdr["Pay_Acc_ID"];
                 u.Pay_Acc_Name = (string)sdr["Pay_Acc_Name"];
+                u.Type = (string)sdr["Type"];
+                u.Tax_Status = (string)sdr["Tax_Status"];
+                u.Rate= (decimal)sdr["Rate"];
+                u.Created_By = (int)sdr["CreatedBy"];
+                u.Created_Date = (DateTime)sdr["CreatedDate"];
+                u.Machine_Ip = (string)sdr["Machine_Ip"];
+                u.Mac_Address = (string)sdr["Mac_Address"];
+                lst.Add(u);
+            }
+            sdr.Close();
+            return lst;
+        }
+        public List<MiddleMan> MiddleMan_Get_By_ID()
+        {
+            List<MiddleMan> lst = new List<MiddleMan>();
+            SqlCommand sc = new SqlCommand("MiddleMan_Get_By_ID", Connection.Get()) { CommandType = System.Data.CommandType.StoredProcedure };
+            sc.Parameters.AddWithValue("@MM_ID", MM_ID);
+            sc.Parameters.AddWithValue("@App_Id", App.App_ID);
+            SqlDataReader sdr = sc.ExecuteReader();
+            while (sdr.Read())
+            {
+                MiddleMan u = new MiddleMan();
+                u.MM_ID = (int)sdr["MM_ID"];
+                u.Trading_Name = (string)sdr["Trading_Name"];
+                u.NTN = (string)sdr["NTN"];
+                u.STRN = (string)sdr["STRN"];
+                u.Contact_Name = (string)sdr["Contact_Name"];
+                u.Phone_Number = (string)sdr["Phone_Number"];
+                u.Rate = (decimal)sdr["Rate"];
+                u.Exp_Acc_ID = (int)sdr["Exp_Acc_ID"];
+                u.Exp_Acc_Name = (string)sdr["Exp_Acc_Name"];
+                u.Pay_Acc_ID = (int)sdr["Pay_Acc_ID"];
+                u.Pay_Acc_Name = (string)sdr["Pay_Acc_Name"];
+                u.Type = (string)sdr["Type"];
+                u.Tax_Status = (string)sdr["Tax_Status"];
+                u.Rate = (decimal)sdr["Rate"];
                 u.Created_By = (int)sdr["CreatedBy"];
                 u.Created_Date = (DateTime)sdr["CreatedDate"];
                 u.Machine_Ip = (string)sdr["Machine_Ip"];
@@ -97,6 +133,9 @@ namespace SfDesk.Models
             sc.Parameters.AddWithValue("@Rate", Rate);
             sc.Parameters.AddWithValue("@Exp_Acc_ID", Exp_Acc_ID);
             sc.Parameters.AddWithValue("@Pay_Acc_ID", Pay_Acc_ID);
+            sc.Parameters.AddWithValue("@Type", Type);
+            sc.Parameters.AddWithValue("@Tax_Status", Tax_Status);
+            sc.Parameters.AddWithValue("@WHT_Rate", WHT_Rate);
             sc.Parameters.AddWithValue("@Machine_Ip", Machine_Ip);
             sc.Parameters.AddWithValue("@Mac_Address", Mac_Address);
             sc.Parameters.AddWithValue("@CreatedBy", App.App_ID);
@@ -115,6 +154,9 @@ namespace SfDesk.Models
             sc.Parameters.AddWithValue("@Rate", Rate);
             sc.Parameters.AddWithValue("@Exp_Acc_ID", Exp_Acc_ID);
             sc.Parameters.AddWithValue("@Pay_Acc_ID", Pay_Acc_ID);
+            sc.Parameters.AddWithValue("@Type", Type);
+            sc.Parameters.AddWithValue("@Tax_Status", Tax_Status);
+            sc.Parameters.AddWithValue("@WHT_Rate", WHT_Rate);
             sc.ExecuteNonQuery();
         }
         public void MiddleMan_Delete()
