cc7dd6757b0a9e733c5d8c119b36ac61a732ae50
Fasih <smfasihali@gmail.com>
2019-07-01 22:02:21 +0500

    123

--- SfDesk/Controllers/PurchaseOrderController.cs
+++ SfDesk/Controllers/PurchaseOrderController.cs
@@ -79,10 +79,12 @@ namespace SfDesk.Controllers
             return Json("delete sucessfully");
         }
         [HttpGet]
+       
         public ActionResult Vehcile_Get_By_Transporter(int id)
         {
             return Json(new Vehicle() { T_ID = id }.Vehcile_Get_By_Transporter(), JsonRequestBehavior.AllowGet);
         }
+        [HttpGet]
         public ActionResult Get_Taxes()
         {
             return Json(new SalesTax().SalesTax_Get_All(), JsonRequestBehavior.AllowGet);
@@ -92,12 +94,55 @@ namespace SfDesk.Controllers
             PI_Charge p = new PI_Charge() { PI_ID = PI_ID };
             return Json(p.PI_Charge_Get_All(), JsonRequestBehavior.AllowGet);
         }
+
+        public ActionResult Get_trans_by_ID(int PI_ID)
+        {
+            PI_Transactions p = new PI_Transactions() { PI_ID = PI_ID };
+            return Json(p.PI_Transactions_Get_All(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Get_Transasction()
+        {
+            List<Transaction> lst = new Transaction().Transaction_Get_All();
+            //lst.Add(new Transaction() { Name = "Middle Man" });
+            //lst.Add(new Transaction() { Name = "Transporter" });
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+
         public ActionResult Save_Charges(int PI_ID, SalesTax s)
         {
             PI_Charge p = new PI_Charge();
             p.PI_ID = PI_ID;
             p.SalesTax_ID = s.SalesTax_ID;
             p.PI_Charge_Add();
+            return Json("");
+        }
+        public ActionResult Save_Transaction(int PI_ID, PI_Transactions s)
+        {
+            s.is_MiddleMan = s.Name.ToLower() == "middle man";
+            s.is_Transporter = s.Name.ToLower() == "transporter";
+
+            s.PI_Transactions_Add();
+            return Json("");
+        }
+        [HttpGet]
+        public ActionResult Mm_Get_By_ID(int id)
+        {
+            return Json(new MiddleMan() { MM_ID = id }.MiddleMan_Get_By_ID(), JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult Vehicle_Get_By_ID(int id)
+        {
+            return Json(new Vehicle() { V_ID = id }.Vehcile_Get_By_ID(), JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult Delete_Charges(PI_Charge s)
+        {
+            s.PI_Charge_Delete();
+            return Json("");
+        }
+        public ActionResult Delete_Transaction(PI_Transactions s)
+        {
+            s.PI_Transactions_Delete();
             return Json("");
         }
     }
