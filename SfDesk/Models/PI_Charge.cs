4950fa5645611186ef20ca2154d39f036c8d2f84
Fasih <smfasihali@gmail.com>
2019-07-25 18:58:57 +0500

    sas

--- SfDesk/Models/PI_Charge.cs
+++ SfDesk/Models/PI_Charge.cs
@@ -13,6 +13,7 @@ namespace SfDesk.Models
         public int SalesTax_ID { get; set; }
         public string SalesTax_Name { get; set; }
         public decimal  Rate { get; set; }
+        public decimal Total { get; set; }
         public string action { get; set; }
         #region default Properties
         public int Created_By { get; set; }
