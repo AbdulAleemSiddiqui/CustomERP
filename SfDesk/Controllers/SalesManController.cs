23aa02f43b6018ea9b9c1d1736d55cd090c5b555
Fasih <smfasihali@gmail.com>
2019-08-28 21:02:29 +0500

    122121

---
+++ SfDesk/Controllers/SalesManController.cs
@@ -0,0 +1,50 @@
+﻿using SfDesk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SfDesk.Controllers
+{
+    public class SalesManController : Controller
+    {
+        // GET: SalesMan
+        public ActionResult Add()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(SalesMan t)
+        {
+
+            t.SM_ID = t.SalesMan_Add();
+            return Json(t, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        public ActionResult Index()
+        {
+            return View(new SalesMan().SalesMan_Get_All());
+        }
+        public ActionResult Update(int id)
+        {
+            return PartialView("Update", new SalesMan() { SM_ID = id }.SalesMan_Get_By_ID());
+
+        }
+        [HttpPost]
+        public ActionResult Update(SalesMan c)
+        {
+            c.SalesMan_Update();
+            return RedirectToAction("index");
+        }
+        public ActionResult Delete(int id)
+        {
+            new SalesMan() { SM_ID = id }.SalesMan_Delete();
+            return RedirectToAction("index");
+        }
+
+    }
+}
---
+++ SfDesk/Models/Salesman.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SfDesk.Models
+{
+    public class SalesMan
+    {
+
+        public int SM_ID { get; set; }
+        public string Trading_Name { get; set; }
+        public string NTN { get; set; }
+        public string STRN { get; set; }
+        public string Contact_Name { get; set; }
+        public string Phone_Number { get; set; }
+        public decimal Rate { get; set; }
+
+        [DisplayName("Expense Account")]
+        public int Exp_Acc_ID { get; set; }
+        public string Exp_Acc_Name { get; set; }
+
+        [DisplayName("Paybal Account")]
+        public int Pay_Acc_ID { get; set; }
+        public string Pay_Acc_Name { get; set; }
+        public string Type { get; set; }
+        public string Tax_Status { get; set; }
+        public decimal WHT_Rate { get; set; }
+
+        #region Default
+        public int Created_By { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Created_Date { get; set; } = DateTime.Parse("2001/01/01");
+        public string Machine_Ip { get; set; }
+        public string Mac_Address { get; set; }
+        public SalesMan()
+        {
+            this.Machine_Ip = Utility.GetIPAddress();
+            this.Mac_Address = Utility.GetMacAddress();
+        }
+
+        #endregion
+
+
+        public List<SalesMan> SalesMan_Get_All()
+        {
+            List<SalesMan> lst = new List<SalesMan>();
+            SqlCommand sc = new SqlCommand("SalesMan_Get_All", Connection.Get()) { CommandType = System.Data.CommandType.StoredProcedure };
+            sc.Parameters.AddWithValue("@App_Id", App.App_ID);
+            SqlDataReader sdr = sc.ExecuteReader();
+            while (sdr.Read())
+            {
+                SalesMan u = new SalesMan();
+                u.SM_ID = (int)sdr["MM_ID"];
+                u.Trading_Name = (string)sdr["Trading_Name"];
+                u.NTN = (string)sdr["NTN"];
+                u.STRN = (string)sdr["STRN"];
+                u.Contact_Name = (string)sdr["Contact_Name"];
+                u.Phone_Number = (string)sdr["Phone_Number"];
+                u.Rate = (decimal)sdr["Rate"];
+                u.Exp_Acc_ID = (int)sdr["Exp_Acc_ID"];
+                u.Exp_Acc_Name = (string)sdr["Exp_Acc_Name"];
+                u.Pay_Acc_ID = (int)sdr["Pay_Acc_ID"];
+                u.Pay_Acc_Name = (string)sdr["Pay_Acc_Name"];
+                //u.Type = (string)sdr["Type"];
+                //u.Tax_Status = (string)sdr["Tax_Status"];
+                u.Rate = (decimal)sdr["Rate"];
+                u.Created_By = (int)sdr["CreatedBy"];
+                u.Created_Date = (DateTime)sdr["CreatedDate"];
+                u.Machine_Ip = (string)sdr["Machine_Ip"];
+                u.Mac_Address = (string)sdr["Mac_Address"];
+                lst.Add(u);
+            }
+            sdr.Close();
+            return lst;
+        }
+        public SalesMan SalesMan_Get_By_ID()
+        {
+            SalesMan u = new SalesMan();
+
+            SqlCommand sc = new SqlCommand("SalesMan_Get_By_ID", Connection.Get()) { CommandType = System.Data.CommandType.StoredProcedure };
+            sc.Parameters.AddWithValue("@MM_ID", SM_ID);
+            sc.Parameters.AddWithValue("@App_Id", App.App_ID);
+            SqlDataReader sdr = sc.ExecuteReader();
+            while (sdr.Read())
+            {
+                u.SM_ID = (int)sdr["MM_ID"];
+                u.Trading_Name = (string)sdr["Trading_Name"];
+                u.NTN = (string)sdr["NTN"];
+                u.STRN = (string)sdr["STRN"];
+                u.Contact_Name = (string)sdr["Contact_Name"];
+                u.Phone_Number = (string)sdr["Phone_Number"];
+                u.Rate = (decimal)sdr["Rate"];
+                u.Exp_Acc_ID = (int)sdr["Exp_Acc_ID"];
+                u.Exp_Acc_Name = (string)sdr["Exp_Acc_Name"];
+                u.Pay_Acc_ID = (int)sdr["Pay_Acc_ID"];
+                u.Pay_Acc_Name = (string)sdr["Pay_Acc_Name"];
+                u.Type = (string)sdr["Type"];
+                u.Tax_Status = (string)sdr["Tax_Status"];
+                u.Rate = (decimal)sdr["Rate"];
+                u.Created_By = (int)sdr["CreatedBy"];
+                u.Created_Date = (DateTime)sdr["CreatedDate"];
+                u.Machine_Ip = (string)sdr["Machine_Ip"];
+                u.Mac_Address = (string)sdr["Mac_Address"];
+            }
+            sdr.Close();
+            return u;
+        }
+        public List<SalesMan> SalesMan_Get_For_LOV()
+        {
+            List<SalesMan> lst = new List<SalesMan>();
+            SqlCommand sc = new SqlCommand("SalesMan_Get_For_LOV", Connection.Get()) { CommandType = System.Data.CommandType.StoredProcedure };
+            sc.Parameters.AddWithValue("@App_Id", App.App_ID);
+            SqlDataReader sdr = sc.ExecuteReader();
+            while (sdr.Read())
+            {
+                SalesMan u = new SalesMan();
+                u.SM_ID = (int)sdr["MM_ID"];
+                u.Trading_Name = (string)sdr["Trading_Name"];
+                u.Rate = (decimal)sdr["Rate"];
+                lst.Add(u);
+            }
+            sdr.Close();
+            return lst;
+        }
+        public int SalesMan_Add()
+        {
+            SqlCommand sc = new SqlCommand("SalesMan_Add", Connection.Get()) { CommandType = System.Data.CommandType.StoredProcedure }; ;
+            sc.Parameters.AddWithValue("@Trading_Name", Trading_Name);
+            sc.Parameters.AddWithValue("@NTN", NTN);
+            sc.Parameters.AddWithValue("@STRN", STRN);
+            sc.Parameters.AddWithValue("@Contact_Name", Contact_Name);
+            sc.Parameters.AddWithValue("@Phone_Number", Phone_Number);
+            sc.Parameters.AddWithValue("@Rate", Rate);
+            sc.Parameters.AddWithValue("@Exp_Acc_ID", Exp_Acc_ID);
+            sc.Parameters.AddWithValue("@Pay_Acc_ID", Pay_Acc_ID);
+            sc.Parameters.AddWithValue("@Type", Type);
+            sc.Parameters.AddWithValue("@Tax_Status", Tax_Status);
+            sc.Parameters.AddWithValue("@WHT_Rate", WHT_Rate);
+            sc.Parameters.AddWithValue("@Machine_Ip", Machine_Ip);
+            sc.Parameters.AddWithValue("@Mac_Address", Mac_Address);
+            sc.Parameters.AddWithValue("@CreatedBy", App.App_ID);
+            object a = sc.ExecuteScalar();
+            return Convert.ToInt32((decimal)a);
+        }
+        public void SalesMan_Update()
+        {
+            SqlCommand sc = new SqlCommand("SalesMan_Update", Connection.Get()) { CommandType = System.Data.CommandType.StoredProcedure }; ;
+            sc.Parameters.AddWithValue("@MM_ID", SM_ID);
+            sc.Parameters.AddWithValue("@Trading_Name", Trading_Name);
+            sc.Parameters.AddWithValue("@NTN", NTN);
+            sc.Parameters.AddWithValue("@STRN", STRN);
+            sc.Parameters.AddWithValue("@Contact_Name", Contact_Name);
+            sc.Parameters.AddWithValue("@Phone_Number", Phone_Number);
+            sc.Parameters.AddWithValue("@Rate", Rate);
+            sc.Parameters.AddWithValue("@Exp_Acc_ID", Exp_Acc_ID);
+            sc.Parameters.AddWithValue("@Pay_Acc_ID", Pay_Acc_ID);
+            sc.Parameters.AddWithValue("@Type", Type);
+            sc.Parameters.AddWithValue("@Tax_Status", Tax_Status);
+            sc.Parameters.AddWithValue("@WHT_Rate", WHT_Rate);
+            sc.ExecuteNonQuery();
+        }
+        public void SalesMan_Delete()
+        {
+            SqlCommand sc = new SqlCommand("SalesMan_Delete", Connection.Get()) { CommandType = System.Data.CommandType.StoredProcedure }; ;
+            sc.Parameters.AddWithValue("@MM_ID", SM_ID);
+            sc.Parameters.AddWithValue("@App_ID", App.App_ID);
+            sc.ExecuteNonQuery();
+        }
+    }
+}
