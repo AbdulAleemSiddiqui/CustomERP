f162a602133b5082c355da1cd8aa5c125b4f197f
Abdul Aleem <abdulaleem.yousuf97@gmail.com>
2019-09-05 18:19:45 +0500

    s

--- SfDesk/Controllers/PurchaseIndendController.cs
+++ SfDesk/Controllers/PurchaseIndendController.cs
@@ -14,5 +14,14 @@ namespace SfDesk.Controllers
         {
             return View(new PI());
         }
+        [HttpPost]
+        public ActionResult Master(PI pi)
+        {
+            return View(new PI());
+        }
+        public ActionResult Get_PI_Detail(int Cat_ID)
+        {
+            return Json(new PI_Detail_ViewModel().PI_Detail_Get_All(Cat_ID) ,JsonRequestBehavior.AllowGet);
+        }
     }
 }
