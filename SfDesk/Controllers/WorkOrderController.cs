5daf285b44f00402f1c161d16ba2bbf77e349abd
Fasih <smfasihali@gmail.com>
2019-10-23 22:31:17 +0500

    EWQEQWE

---
+++ SfDesk/Controllers/WorkOrderController.cs
@@ -0,0 +1,36 @@
+﻿using SfDesk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SfDesk.Controllers
+{
+    public class WorkOrderController : Controller
+    {
+        // GET: WorkOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Master()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Master(WorkOrder c)
+        {
+            return View();
+        }
+
+        public ActionResult COA_Get_All()
+        {
+            return Json(new ChartOfAccount().COA_Get_All(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Product_Get_All()
+        {
+            return Json(new Item_Category().Item_Category_Get_All(App.App_ID), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ SfDesk/Models/WorkOrder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SfDesk.Models
+{
+    public class WorkOrder
+    {
+        public int WO_ID { get; set; }
+        public string WO_NO { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Due_Date { get; set; }
+        public double Total { get; set; }
+        public int Quantity { get; set; }
+        public string  Reference { get; set; }
+        public List<Recipe_Detail> Input_products { get; set; }
+        public List<Recipe_Detail> Output_products { get; set; }
+        public List<Recipe_Expense> Account_expences { get; set; }
+    }
+    public class WO_Detail
+    {
+    
+        public int WOD_ID { get; set; }
+
+      
+        public int WO_ID { get; set; }
+        
+        public int Item_ID { get; set; }
+        
+        public string Description { get; set; }
+       
+        public int Quantity { get; set; }
+        
+        public string Flag { get; set; }
+
+        private const string Module = "";
+
+        //Your Properties for Model Here
+
+
+        public int CreatedBy { get; set; }
+
+        public string ReturnMessage { get; set; }
+
+
+
+
+    }
+    public class WO_Expense
+    {
+        private const string Module = "";
+
+       
+        public int WOE_ID { get; set; }
+       
+       
+        public int COA_ID { get; set; }
+        public string COA_Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public decimal Amount { get; set; }
+        
+        public int CreatedBy { get; set; }
+
+
+        //View Only Properties
+        public string ReturnMessage { get; set; }
+
+
+
+    }
+}
